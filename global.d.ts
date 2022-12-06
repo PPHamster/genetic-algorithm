0eff665e89225150e4727552babdd5f285175682
PPHamster <shinnapatjr@gmail.com>
2022-12-07 00:43:19 +0700

    refactor: improve project setting

--- global.d.ts
+++ global.d.ts
@@ -3,12 +3,16 @@ declare global {
     interface ProcessEnv {
       NODE_ENV: 'development' | 'production';
 
-      POPULATION_SIZE: number,
-      CUSTOMER_COUNT: number,
-      WEIGHT_LIMIT: number,
-      CROSSOVER_RATE: number,
-      MUTATION_RATE: number,
-      NUMBER_OF_RUN: number,
+      POPULATION_SIZE: number;
+      CUSTOMER_COUNT: number;
+      MAX_DISTANCE: number;
+      WEIGHT_LIMIT: number;
+      CROSSOVER_RATE: number;
+      MUTATION_RATE: number;
+      NUMBER_OF_RUN: number;
+      DEMAND_CHOOSEN: number;
+      TERMINAL_CRITERIA: number;
+      NO_IMPROVEMENT: number;
     }
   }
 }
--- src/packages/type.ts
+++ src/packages/type.ts
@@ -1,10 +1,14 @@
 export type Constance = {
   POPULATION_SIZE: number,
   CUSTOMER_COUNT: number,
+  MAX_DISTANCE: number,
   WEIGHT_LIMIT: number,
   CROSSOVER_RATE: number,
   MUTATION_RATE: number,
   NUMBER_OF_RUN: number,
+  DEMAND_CHOOSEN: number,
+  TERMINAL_CRITERIA: number,
+  NO_IMPROVEMENT: number,
 }
 
 export type Population = {
@@ -18,4 +22,5 @@ export type Generation = {
   minPath: number[],
   minDistance: number,
   average: number,
+  vehicleRun: number[],
 }
